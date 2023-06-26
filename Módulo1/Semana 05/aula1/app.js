8fb29f92d442c17975969e527c167a95560bb3b6
maxmilianand <abc.max@hotmail.com>
2023-06-26 20:54:44 -0300

    Manipulando elemento com js

--- Módulo1/Semana 05/aula1/app.js
+++ Módulo1/Semana 05/aula1/app.js
@@ -1,1 +1,12 @@
-console.log("m1s05a1")
+console.log("m1s05a1");
+
+// Manipular o primeiro elemento h1:
+let h1 = document.getElementsByTagName("h1")[0];
+h1.style.color = "#0A0";
+
+
+// Criar uma query com todos os elementos h2 no html:
+let h1List = document.querySelectorAll("h2");
+h1List.forEach(h2 => {
+  h2.style.color = "#CCC";
+});
