81bf74caa5c4b0137c3014d2a911b8f447edd16c
maxmilianand <abc.max@hotmail.com>
2023-07-14 13:01:26 -0300

    ex2

---
+++ Módulo1/Semana 07/Exercicios/ex2/Fatura.js
@@ -0,0 +1,18 @@
+class Fatura {
+  constructor(id, descricao, quantia, preco) {
+    this.id = id;
+    this.descricao = descricao;
+    this.quantia = quantia > 0 ? quantia : 0;
+    this.preco = preco > 0 ? preco : 0;
+  }
+
+  obterValorTotal() {
+    return this.quantia * this.preco;
+  }
+
+  get valorTotal() {
+    return this.quantia * this.preco;
+  }
+}
+
+export default Fatura;
